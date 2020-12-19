5aa8ae62d6afa1f9f4c589b3b87c799548d4d8f7
Administrator <Administrator@192.168.0.108>
2020-12-19 16:17:36 +0800

    认识Frame类

---
+++ tank/src/com/tank/tank.java
@@ -0,0 +1,26 @@
+package com.tank;
+
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class tank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Frame f=new Frame();  //farme类
+		f.setTitle("tank war");
+		f.setSize(600,500);
+		f.setResizable(false);
+		f.setVisible(true);
+		//window监听器
+		f.addWindowListener(new WindowAdapter(){//匿名类
+			@Override
+			public void windowClosing(WindowEvent e) {
+				// TODO Auto-generated method stub
+                 System.exit(0);
+			}	
+		});
+	}
+
+}
